3eac6641957c32ce275f229daebb3be89dc3f280
Marco Milano <mrc.milano@gmail.com>
2026-05-27 14:25:54 +0200

    chore: add reference to original author in .ts

--- kilo.ts
+++ kilo.ts
@@ -5,8 +5,10 @@
  * Uses device code flow for browser-based authentication.
  *
  * Usage:
- *   pi install git:github.com/mrexodia/kilo-pi-provider
+ *   pi install git:github.com/mrcmilano/kilo-pi-provider
  *   # Then /login kilo, or set KILO_API_KEY=...
+ *
+ * Adapted from: https://github.com/mrexodia/kilo-pi-provider
  */
 
 import type {
